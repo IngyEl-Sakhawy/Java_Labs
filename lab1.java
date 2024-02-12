cc595e156b6854503ee5b7f6b7851f9c58c5bd40
ingy <ingy.mahmoud.elsakhawy@gmail.com>
2024-02-12 15:09:17 +0200

    Java Labs

---
+++ lab1.java
@@ -0,0 +1,15 @@
+/**
+our first java lab includes three examples
+*/
+
+
+class Lab1{
+	
+	
+	public static void main(String args[]) {
+		
+System.out.println("This is a simple Java program.");
+
+}
+}
+
